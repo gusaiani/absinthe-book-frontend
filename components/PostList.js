9d4d3898f2ed38f58d588935ac40698c016d0da3
Gustavo Saiani <gustavo@poe.ma>
2018-08-20 17:01:17 -0300

    Minimal call to GraphQL endpoint rendering menuItems

--- components/PostList.js
+++ components/PostList.js
@@ -1,38 +1,27 @@
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import ErrorMessage from './ErrorMessage'
-import PostUpvoter from './PostUpvoter'
 
 const POSTS_PER_PAGE = 10
 
 function PostList ({
-  data: { loading, error, allPosts, _allPostsMeta },
+  data: {error, menuItems},
   loadMorePosts
 }) {
   if (error) return <ErrorMessage message='Error loading posts.' />
-  if (allPosts && allPosts.length) {
-    const areMorePosts = allPosts.length < _allPostsMeta.count
+
+  if (menuItems) {
     return (
       <section>
         <ul>
-          {allPosts.map((post, index) => (
-            <li key={post.id}>
+          {menuItems.map((menuItem, index) => (
+            <li key={menuItem.name}>
               <div>
-                <span>{index + 1}. </span>
-                <a href={post.url}>{post.title}</a>
-                <PostUpvoter id={post.id} votes={post.votes} />
+                <p>{menuItem.name}</p>
               </div>
             </li>
           ))}
         </ul>
-        {areMorePosts ? (
-          <button onClick={() => loadMorePosts()}>
-            {' '}
-            {loading ? 'Loading...' : 'Show More'}{' '}
-          </button>
-        ) : (
-          ''
-        )}
         <style jsx>{`
           section {
             padding-bottom: 20px;
@@ -78,49 +67,20 @@ function PostList ({
 }
 
 export const allPosts = gql`
-  query allPosts($first: Int!, $skip: Int!) {
-    allPosts(orderBy: createdAt_DESC, first: $first, skip: $skip) {
-      id
-      title
-      votes
-      url
-      createdAt
-    }
-    _allPostsMeta {
-      count
+  query {
+    menuItems {
+      name
     }
   }
 `
-export const allPostsQueryVars = {
-  skip: 0,
-  first: POSTS_PER_PAGE
-}
 
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (PostList)
 export default graphql(allPosts, {
-  options: {
-    variables: allPostsQueryVars
-  },
+  options: {},
   props: ({ data }) => {
     return ({
-      data,
-      loadMorePosts: () => {
-        return data.fetchMore({
-          variables: {
-            skip: data.allPosts.length
-          },
-          updateQuery: (previousResult, { fetchMoreResult }) => {
-            if (!fetchMoreResult) {
-              return previousResult
-            }
-            return Object.assign({}, previousResult, {
-              // Append the new posts results to the old one
-              allPosts: [...previousResult.allPosts, ...fetchMoreResult.allPosts]
-            })
-          }
-        })
-      }
+      data
     })
   }
 })(PostList)
